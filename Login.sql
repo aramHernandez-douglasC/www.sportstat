37638a7d8ded0fb97ba43e100e9ac1fbd04ed6c0
lracoma <60127171+lracoma@users.noreply.github.com>
2020-02-24 11:59:39 -0800

    Add files via upload
    Individual SQL insert

---
+++ Login.sql
@@ -0,0 +1,5 @@
+insert into Login (_loginUser, _loginPassword) values ('tscolts0', 'kNnJFjwEP3O');
+insert into Login (_loginUser, _loginPassword) values ('dmerrin1', '6ZDsrkU');
+insert into Login (_loginUser, _loginPassword) values ('mmccard2', 'vEC7B8g2e');
+insert into Login (_loginUser, _loginPassword) values ('ralner3', '0rgttSdFkpCf');
+insert into Login (_loginUser, _loginPassword) values ('tdowyer4', 'qA3xS5');
---
+++ Player.sql
@@ -0,0 +1,10 @@
+insert into Player (_playerFirstName, _playerLastName, _playerPosition, _playerHeight, _playerWeight, _playerDob, _playerAge, _playerNumber) values ('Jethro', 'Jaime', 'PH', 179, 334, '7/9/1991', 35, 99);
+insert into Player (_playerFirstName, _playerLastName, _playerPosition, _playerHeight, _playerWeight, _playerDob, _playerAge, _playerNumber) values ('Evy', 'Minichi', 'PT', 218, 486, '10/3/1987', 45, 51);
+insert into Player (_playerFirstName, _playerLastName, _playerPosition, _playerHeight, _playerWeight, _playerDob, _playerAge, _playerNumber) values ('Cati', 'Raulstone', 'CN', 187, 445, '5/27/1978', 20, 7);
+insert into Player (_playerFirstName, _playerLastName, _playerPosition, _playerHeight, _playerWeight, _playerDob, _playerAge, _playerNumber) values ('Hamish', 'Langham', 'FR', 192, 375, '5/30/1980', 33, 87);
+insert into Player (_playerFirstName, _playerLastName, _playerPosition, _playerHeight, _playerWeight, _playerDob, _playerAge, _playerNumber) values ('Bernelle', 'Gullifant', 'CL', 179, 460, '12/12/1990', 49, 92);
+insert into Player (_playerFirstName, _playerLastName, _playerPosition, _playerHeight, _playerWeight, _playerDob, _playerAge, _playerNumber) values ('Ki', 'Goodspeed', 'PH', 220, 313, '12/30/1971', 48, 77);
+insert into Player (_playerFirstName, _playerLastName, _playerPosition, _playerHeight, _playerWeight, _playerDob, _playerAge, _playerNumber) values ('Dinny', 'Hutsby', 'CN', 173, 332, '12/2/1977', 46, 49);
+insert into Player (_playerFirstName, _playerLastName, _playerPosition, _playerHeight, _playerWeight, _playerDob, _playerAge, _playerNumber) values ('Abbey', 'Dyshart', 'RU', 197, 332, '10/31/1997', 41, 11);
+insert into Player (_playerFirstName, _playerLastName, _playerPosition, _playerHeight, _playerWeight, _playerDob, _playerAge, _playerNumber) values ('Teresa', 'Lynett', 'CA', 220, 209, '10/29/1970', 42, 67);
+insert into Player (_playerFirstName, _playerLastName, _playerPosition, _playerHeight, _playerWeight, _playerDob, _playerAge, _playerNumber) values ('Magnum', 'Manske', 'CN', 207, 290, '3/8/1994', 44, 5);
