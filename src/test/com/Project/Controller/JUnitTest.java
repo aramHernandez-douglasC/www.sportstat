85426e495e8a65c46037882f151e915b70b92fd3
GeekedAt-Birth <ritasiima612@outlook.com>
2020-03-17 07:30:15 -0700

    Added Junit Test

---
+++ src/test/com/Project/Controller/JUnitTest.java
@@ -0,0 +1,30 @@
+package test.com.Project.Controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.project.Controller.LoginService;
+import com.project.Entity.Login;
+
+import jdk.internal.org.objectweb.asm.util.CheckAnnotationAdapter;
+
+class JUnitTest {
+     
+	LoginService testLoginService;
+	Login testUserLogin; 
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		testUserLogin = new Login("bro","ham");
+		
+	}
+
+	@Test
+	void testToCheckAuthentication() {
+		assertEquals(" Test is a success", testLoginService.checkAuthentication(testUserLogin));
+	}
+
+}
