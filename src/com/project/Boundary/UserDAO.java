6f01f8b275e8a49d0f3a25dd25ffa8616bd315c8
Aram <aramhg@outlook.com>
2020-03-17 01:54:44 -0700

    UserDAO added, substitutes LoginDAO

---
+++ src/com/project/Boundary/UserDAO.java
@@ -0,0 +1,226 @@
+package com.project.Boundary;
+
+import java.sql.SQLException;
+
+
+
+import com.project.Entity.User;
+
+public class UserDAO extends DatabaseHelper {
+	
+public void addInfo(User p) {
+		
+		// Add a new player
+		String sql = "insert into User (_fullName, _dob, _city, _province, _country, _loginUser, _loginPassword)" + "VALUES(?,?,?,?,?,?,?)";
+
+		try {
+			connectDB();
+
+			this.prepStatement = this.connect.prepareStatement(sql);
+
+			this.prepStatement.setString(1, p.get_fullName());
+			this.prepStatement.setString(2, p.get_dob().toString());
+			this.prepStatement.setString(3, p.get_city());
+			this.prepStatement.setString(4, p.get_province());
+			this.prepStatement.setString(5, p.get_country());
+			this.prepStatement.setString(6, p.get_loginUser());
+			this.prepStatement.setString(7, p.get_loginPassword());
+			
+			
+
+			this.prepStatement.execute();
+
+			System.out.println("User Added! : " + p.get_userID());
+			disconnectDB();
+
+		} catch (SQLException m) {
+			System.out.println("Error Adding User...");
+			System.out.println(m.getMessage());
+			System.out.println(m.getErrorCode());
+			System.out.println(m.getSQLState());
+		}
+
+	}
+
+
+
+public void updateName(User u, String userName) {
+
+		
+		// Update Query
+		String sql = "UPDATE User SET " + "_fullName= ?" + "WHERE _loginUser = " + userName;
+		
+		try {
+
+			connectDB();
+
+			// Create the prepared statements
+			this.prepStatement = this.connect.prepareStatement(sql);
+
+			// Execute the statement
+			this.prepStatement.executeUpdate();
+
+			// Set the parameters
+			
+			this.prepStatement.setString(1, u.get_fullName());
+			
+
+			System.out.println("Updated User: " + u.get_userID());
+
+			// Disconnect to the database
+			disconnectDB();
+
+		} catch (SQLException m) {
+
+			System.out.println("Error updating user fullname");
+			System.out.println(m.getMessage());
+			System.out.println(m.getErrorCode());
+			System.out.println(m.getSQLState());
+		}
+
+	}
+
+
+public void updateDOB(User u, String userName) {
+
+		
+		// Update Query
+		String sql = "UPDATE User SET " + "_dob= ?" + "WHERE _loginUser = " + userName;
+		
+		try {
+
+			connectDB();
+
+			// Create the prepared statements
+			this.prepStatement = this.connect.prepareStatement(sql);
+
+			// Execute the statement
+			this.prepStatement.executeUpdate();
+
+			// Set the parameters
+			
+			this.prepStatement.setString(1, u.get_dob().toString());
+			
+
+			System.out.println("Updated User: " + u.get_userID());
+
+			// Disconnect to the database
+			disconnectDB();
+
+		} catch (SQLException m) {
+
+			System.out.println("Error updating user birthday");
+			System.out.println(m.getMessage());
+			System.out.println(m.getErrorCode());
+			System.out.println(m.getSQLState());
+		}
+
+	}
+
+public void updateUserName(User u, String userName) {
+
+	
+	// Update Query
+	String sql = "UPDATE User SET " + "_loginUser= ?" + "WHERE _loginUser = " + userName;
+	
+	try {
+
+		connectDB();
+
+		// Create the prepared statements
+		this.prepStatement = this.connect.prepareStatement(sql);
+
+		// Execute the statement
+		this.prepStatement.executeUpdate();
+
+		// Set the parameters
+		
+		this.prepStatement.setString(1, u.get_loginUser());
+		
+
+		System.out.println("Updated User: " + u.get_userID());
+
+		// Disconnect to the database
+		disconnectDB();
+
+	} catch (SQLException m) {
+
+		System.out.println("Error updating username");
+		System.out.println(m.getMessage());
+		System.out.println(m.getErrorCode());
+		System.out.println(m.getSQLState());
+	}
+
+}
+
+public void updatePassword(User u, String userName) {
+
+	
+	// Update Query
+	String sql = "UPDATE User SET " + "_loginPassword= ?" + "WHERE _loginUser = " + userName;
+	
+	try {
+
+		connectDB();
+
+		// Create the prepared statements
+		this.prepStatement = this.connect.prepareStatement(sql);
+
+		// Execute the statement
+		this.prepStatement.executeUpdate();
+
+		// Set the parameters
+		
+		this.prepStatement.setString(1, u.get_loginPassword());
+		
+
+		System.out.println("Updated User: " + u.get_userID());
+
+		// Disconnect to the database
+		disconnectDB();
+
+	} catch (SQLException m) {
+
+		System.out.println("Error updating user password");
+		System.out.println(m.getMessage());
+		System.out.println(m.getErrorCode());
+		System.out.println(m.getSQLState());
+	}
+
+}
+
+
+	public void deleteInfo(User id) {
+
+		try {
+
+			connectDB();
+
+			// Delete query
+			String sql = "DELETE FROM Team WHERE id=?";
+
+			this.prepStatement = this.connect.prepareStatement(sql);
+
+			this.prepStatement.setInt(1, id.get_userID());
+
+			// Execute the query
+			this.prepStatement.executeUpdate();
+
+			System.out.println("Deleted Team: " + id.get_userID());
+
+			disconnectDB();
+		} catch (SQLException m) {
+
+			System.out.println("Error deleting Team");
+			System.out.println(m.getMessage());
+			System.out.println(m.getErrorCode());
+			System.out.println(m.getSQLState());
+		}
+	}
+	
+	
+	
+}
+
+
+
--- src/com/project/Entity/User.java
+++ src/com/project/Entity/User.java
@@ -1,5 +1,82 @@
 package com.project.Entity;
 
-public class User {
+import java.util.Date;
 
+public class User {
+	
+ 
+	private int _userID;
+    private String _fullName;
+    private Date _dob;
+    private String _city;
+    private String _province; 
+    private String _country;
+    private String _loginUser; 
+    private String _loginPassword;
+    
+    public User(int _userID, String _fullName, Date _dob, String _city, String _province, String _country,
+			String _loginUser, String _loginPassword) {
+		super();
+		this._userID = _userID;
+		this._fullName = _fullName;
+		this._dob = _dob;
+		this._city = _city;
+		this._province = _province;
+		this._country = _country;
+		this._loginUser = _loginUser;
+		this._loginPassword = _loginPassword;
+	}
+    
+	public int get_userID() {
+		return _userID;
+	}
+	public void set_userID(int _userID) {
+		this._userID = _userID;
+	}
+	public String get_fullName() {
+		return _fullName;
+	}
+	public void set_fullName(String _fullName) {
+		this._fullName = _fullName;
+	}
+	public Date get_dob() {
+		return _dob;
+	}
+	public void set_dob(Date _dob) {
+		this._dob = _dob;
+	}
+	public String get_city() {
+		return _city;
+	}
+	public void set_city(String _city) {
+		this._city = _city;
+	}
+	public String get_province() {
+		return _province;
+	}
+	public void set_province(String _province) {
+		this._province = _province;
+	}
+	public String get_country() {
+		return _country;
+	}
+	public void set_country(String _country) {
+		this._country = _country;
+	}
+	public String get_loginUser() {
+		return _loginUser;
+	}
+	public void set_loginUser(String _loginUser) {
+		this._loginUser = _loginUser;
+	}
+	public String get_loginPassword() {
+		return _loginPassword;
+	}
+	public void set_loginPassword(String _loginPassword) {
+		this._loginPassword = _loginPassword;
+	}
 }
+	
+	
+    
+	
